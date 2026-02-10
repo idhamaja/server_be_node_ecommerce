bd4af51aaad17c1d4f3dddd993d050f2d0e38b14
Idham Ganteng <idham16iramap.com>
2026-02-10 10:29:52 +0700

    update now

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello Idham Ganteng")
+const express = require("express");
+require("dotenv/config");
+
+const app = express();
+const env = process.env;
+
+app.get("/", (request, response) => {
+  return response.send("<h1>Welcome Idham Ganteng </h1>");
+});
+
+const hostname = process.env.HOSTNAME;
+const port = env.PORT;
+
+mongoose.connect();
+
+//start the server
+app.listen(port, hostname, () => {
+  console.log(`server is running at  http://${hostname}:${port}`);
+});
